71a60e38146b8f7ec27841e3c49ed0e8e734c475
L47-Coder <2360980084@qq.com>
2026-04-29 12:10:16 +0800

    版本前的改动

--- Assets/Game/Frame/GameBoot.cs
+++ Assets/Game/Frame/GameBoot.cs
@@ -5,19 +5,8 @@ using VContainer;
 
 public class GameBoot : MonoBehaviour, IGameBoot
 {
-    [Inject] private readonly IPrefabManager _prefabManager;
     public async UniTask OnGameStart()
     {
-        var handle = await _prefabManager.LoadPrefabAsync("Ground");
-
-        await UniTask.Delay(2000);
-
-        await _prefabManager.ReleasePrefabAsync(handle);
-
-        await UniTask.Delay(2000);
-
-        await _prefabManager.LoadPrefabAsync("Ground");
-
         await UniTask.CompletedTask;
     }
 }
